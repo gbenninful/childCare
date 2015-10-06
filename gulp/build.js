641b0914948651c182244c2dcacc9f4145d2a09e
Jeff Enninful <jeffenninful@yahoo.com>
2015-10-06 01:37:34 -0700

    run task in sequence

--- gulp/build.js
+++ gulp/build.js
@@ -1,14 +1,13 @@
 'use strict';
 
 var gulp = require('gulp'),
+    runSequence = require('run-sequence').use(gulp),
     config = require('../gulpConfig')(),
     $ = require('gulp-load-plugins')({
         lazy: true,
         camelize: true
     });
 
-//runSequence('clean', 'lint', 'browserify',  'styles', 'wiredep','inject', 'serve');
-
-gulp.task('build', ['clean', 'browserify', 'lint', 'styles', 'wiredep'], function () {
-    gulp.start('inject', 'serve');
+gulp.task('build', function (done) {
+    runSequence('clean', 'lint', 'browserify', 'styles', 'wiredep', 'inject', 'serve', done);
 });
